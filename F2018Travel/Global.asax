31613bc001675996efc6e98b2c3cdbe3e84b8cbf
ifotn <rich@infrontofthenet.com>
2018-12-09 10:16:22 -0500

    Add project files.

---
+++ F2018Travel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="F2018Travel.MvcApplication" Language="C#" %>
---
+++ F2018Travel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(F2018Travel.Startup))]
+namespace F2018Travel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
